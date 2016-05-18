c19ce1ada703570d86d567d07fcbddaf05edbc7d
Bertrand Martel <bmartel.fr@gmail.com>
2016-05-19 00:05:17 +0200

    add package info

---
+++ examples/src/main/java/fr/bmartel/speedtest/examples/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Examples package for Speed Test Library.
+ * <p/>
+ *
+ * @author Bertrand Martel
+ */
+package fr.bmartel.speedtest.examples;
---
+++ speedtest/src/test/java/fr/bmartel/speedtest/examples/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Speed test library unit test package.
+ * <p/>
+ *
+ * @author Bertrand Martel
+ */
+package fr.bmartel.speedtest.examples;
