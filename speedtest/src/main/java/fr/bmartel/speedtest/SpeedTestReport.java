52dea779ec74071726a015874189d8ada4566bb4
Bertrand Martel <bmartel.fr@gmail.com>
2016-09-27 21:51:16 +0200

    fix temporarypacketSize type in report object

--- speedtest/src/main/java/fr/bmartel/speedtest/SpeedTestReport.java
+++ speedtest/src/main/java/fr/bmartel/speedtest/SpeedTestReport.java
@@ -118,7 +118,7 @@ public class SpeedTestReport {
      *
      * @return packet size in bit
      */
-    public float getTemporaryPacketSize() {
+    public long getTemporaryPacketSize() {
         return tempPacketSize;
     }
 
