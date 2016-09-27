e8248a9c752ac9f1c9faa61d1e8f79fec1ad6e19
Bertrand Martel <bmartel.fr@gmail.com>
2016-09-27 07:31:04 +0200

    unit test : socket + mode

--- speedtest/src/test/java/fr/bmartel/speedtest/examples/SpeedTestSocketTest.java
+++ speedtest/src/test/java/fr/bmartel/speedtest/examples/SpeedTestSocketTest.java
@@ -24,10 +24,16 @@
 
 package fr.bmartel.speedtest.examples;
 
-import fr.bmartel.speedtest.SpeedTestSocket;
-
+import fr.bmartel.speedtest.*;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Speed examples socket examples.
@@ -70,6 +76,31 @@ public class SpeedTestSocketTest {
      * invalid value for upload chunk packet size.
      */
     private static final int UPLOAD_CHUNK_INVALID = 30000;
+
+    /**
+     * speed examples server host name.
+     */
+    private final static String SPEED_TEST_SERVER_HOST = "2.testdebit.info";
+
+    /**
+     * spedd examples server uri.
+     */
+    private final static String SPEED_TEST_SERVER_URI_DL = "/fichiers/100Mo.dat";
+
+    /**
+     * speed examples server port.
+     */
+    private final static int SPEED_TEST_SERVER_PORT = 80;
+
+    /**
+     * spedd examples server uri.
+     */
+    private static final String SPEED_TEST_SERVER_URI_UL = "/";
+
+    /**
+     * upload 10Mo file size.
+     */
+    private static final int FILE_SIZE = 1000000;
 
     /**
      * test socket timeout default value.
@@ -125,4 +156,180 @@ public class SpeedTestSocketTest {
                 UPLOAD_CHUNK_INVALID);
     }
 
+    /**
+     * test rounding mode setter valid value.
+     */
+    @Test
+    public void defaultRoundingModeSetterTest() {
+        socket = new SpeedTestSocket();
+        socket.setDefaultRoundingMode(RoundingMode.HALF_UP);
+        Assert.assertEquals(HEADER + "rounding mode incorrect value after set", socket.getDefaultRoundingMode(),
+                RoundingMode.HALF_UP);
+    }
+
+    /**
+     * test scale setter valid value.
+     */
+    @Test
+    public void defaultScaleSetterTest() {
+        socket = new SpeedTestSocket();
+        socket.setDefaultScale(8);
+        Assert.assertEquals(HEADER + "scale incorrect value after set", socket.getDefaultScale(),
+                8);
+    }
+
+    /**
+     * test speed test mode value.
+     */
+    @Test
+    public void speedTestModeTest() {
+        socket = new SpeedTestSocket();
+        Assert.assertEquals(HEADER + "speed test mode value after init", socket.getSpeedTestMode(),
+                SpeedTestMode.NONE);
+        socket.startDownload(SPEED_TEST_SERVER_HOST, SPEED_TEST_SERVER_PORT, SPEED_TEST_SERVER_URI_DL);
+        Assert.assertEquals(HEADER + "speed test mode value after startDownload", socket.getSpeedTestMode(),
+                SpeedTestMode.DOWNLOAD);
+        socket.forceStopTask();
+        Assert.assertEquals(HEADER + "speed test mode value after forceStopTask", socket.getSpeedTestMode(),
+                SpeedTestMode.NONE);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                socket.startUpload(SPEED_TEST_SERVER_HOST, SPEED_TEST_SERVER_PORT, SPEED_TEST_SERVER_URI_UL,
+                        FILE_SIZE);
+            }
+        }).start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(HEADER + "speed test mode value after startUpload", socket.getSpeedTestMode(),
+                SpeedTestMode.UPLOAD);
+        socket.forceStopTask();
+        Assert.assertEquals(HEADER + "speed test mode value after forceStopTask", socket.getSpeedTestMode(),
+                SpeedTestMode.NONE);
+    }
+
+    /**
+     * test listener object.
+     */
+    @Test
+    public void listenerTest() {
+        socket = new SpeedTestSocket();
+
+        final List<ISpeedTestListener> listenerList = new ArrayList<ISpeedTestListener>();
+
+        final ISpeedTestListener listener = new ISpeedTestListener() {
+            @Override
+            public void onDownloadPacketsReceived(final long packetSize, final BigDecimal transferRateBps,
+                                                  final BigDecimal transferRateOps) {
+
+            }
+
+            @Override
+            public void onDownloadProgress(final float percent, final SpeedTestReport report) {
+
+            }
+
+            @Override
+            public void onDownloadError(final SpeedTestError speedTestError, final String errorMessage) {
+
+            }
+
+            @Override
+            public void onUploadPacketsReceived(final long packetSize, final BigDecimal transferRateBps,
+                                                final BigDecimal transferRateOps) {
+
+            }
+
+            @Override
+            public void onUploadError(final SpeedTestError speedTestError, final String errorMessage) {
+
+            }
+
+            @Override
+            public void onUploadProgress(final float percent, final SpeedTestReport report) {
+
+            }
+        };
+
+        try {
+            final Field field = socket.getClass().getDeclaredField("listenerList");
+            Assert.assertNotNull(HEADER + "listenerList is null", field);
+            field.setAccessible(true);
+            field.set(socket, listenerList);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        socket.addSpeedTestListener(listener);
+
+        Assert.assertEquals(HEADER + "listener add failed", listenerList.size(), 1);
+
+        socket.removeSpeedTestListener(listener);
+
+        Assert.assertEquals(HEADER + "listener remove failed", listenerList.size(), 0);
+    }
+
+    /**
+     * test socket valid value.
+     */
+    @Test
+    public void socketTest() {
+        socket = new SpeedTestSocket();
+
+        try {
+            final Field field = socket.getClass().getDeclaredField("socket");
+            Assert.assertNotNull(HEADER + "socket is null", field);
+            field.setAccessible(true);
+
+            Assert.assertNull(HEADER + "socket value at init", field.get(socket));
+
+            socket.startDownload(SPEED_TEST_SERVER_HOST, SPEED_TEST_SERVER_PORT, SPEED_TEST_SERVER_URI_DL);
+            Thread.sleep(500);
+            Assert.assertNotNull(HEADER + "socket value after download", field.get(socket));
+            Assert.assertTrue(HEADER + "socket connected after download", ((Socket) field.get(socket)).isConnected());
+            Assert.assertFalse(HEADER + "socket closed after download", ((Socket) field.get(socket)).isClosed());
+
+            socket.forceStopTask();
+            Thread.sleep(500);
+            Assert.assertTrue(HEADER + "socket closed after stop download", ((Socket) field.get(socket)).isClosed());
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    socket.startUpload(SPEED_TEST_SERVER_HOST, SPEED_TEST_SERVER_PORT, SPEED_TEST_SERVER_URI_UL,
+                            FILE_SIZE);
+                }
+            }).start();
+
+            Thread.sleep(500);
+            Assert.assertNotNull(HEADER + "socket value after upload", field.get(socket));
+            Assert.assertTrue(HEADER + "socket connected after upload", ((Socket) field.get(socket)).isConnected());
+            Assert.assertFalse(HEADER + "socket closed after upload", ((Socket) field.get(socket)).isClosed());
+
+            socket.forceStopTask();
+            Thread.sleep(500);
+            Assert.assertTrue(HEADER + "socket closed after stop upload", ((Socket) field.get(socket)).isClosed());
+
+            socket.startDownload(SPEED_TEST_SERVER_HOST, SPEED_TEST_SERVER_PORT, SPEED_TEST_SERVER_URI_DL);
+            Thread.sleep(500);
+            Assert.assertNotNull(HEADER + "socket value after download", field.get(socket));
+            Assert.assertTrue(HEADER + "socket connected after download", ((Socket) field.get(socket)).isConnected());
+            Assert.assertFalse(HEADER + "socket closed after download", ((Socket) field.get(socket)).isClosed());
+            socket.closeSocket();
+            Assert.assertTrue(HEADER + "socket closed after stop download", ((Socket) field.get(socket)).isClosed());
+
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
